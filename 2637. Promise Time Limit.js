7826ed3a22deadbe7a40d8eeca6828f403ddeab8
Shivam Basak <90944419+shivamBasak@users.noreply.github.com>
2024-03-14 06:34:42 +0530

    23

---
+++ 2637. Promise Time Limit.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        const tP=new Promise((resolve,reject)=>{
+            setTimeout(()=>reject("Time Limit Exceeded"),t);
+        });
+        return await Promise.race([fn(...args),tP]);
+    };
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
